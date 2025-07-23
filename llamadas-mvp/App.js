08076b8791a60fb9f22f3528d6d1eb4d163f261a
AlejoZero22 <alejo.apolo36@gmail.com>
2025-07-22 19:31:52 -0500

    Se Agregan Nombres y Numeros
    Se agregaron Nombres de personajes de juegos y sus numeros de telefono/celular

--- llamadas-mvp/App.js
+++ llamadas-mvp/App.js
@@ -18,3 +18,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+return (
+  <view style={styles.container}>
+    <text style={styles.title}>{appTitle}</text>
+    <tex style={styles.title}>Zero - 0204082480</tex>
+    <tex style={styles.title}>Link - 6589478526</tex>
+    <tex style={styles.title}>Don Celular muerta - 6453597850</tex>
+    <tex style={styles.title}>Mega X - 2648975605 </tex>
+    <tex style={styles.title}>Mario - 3689745821</tex>
+
+
+  </view>
+);
+
